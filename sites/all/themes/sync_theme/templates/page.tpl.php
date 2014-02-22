112d0f9e242bb76f11e29516a73e7a7a789e0f73
devildeveloper <devx.ry@gmail.com>
2014-02-22 02:31:39 -0500

    probando enlaces

--- sites/all/themes/sync_theme/templates/page.tpl.php
+++ sites/all/themes/sync_theme/templates/page.tpl.php
@@ -519,13 +519,13 @@
                 <li><span class="phone">Telefono: 941-377-609</span></li>
             </ul>
             
-            <div class="footer-social"><a href="http://facebook.com/" target="_blank"><img src="images/footer_facebook.png" alt=""></a></div>
-            <div class="footer-social"><a href="http://twitter.com/" target="_blank"><img src="images/footer_twitter.png" alt=""></a></div>
-            <div class="footer-social"><a href="http://google.com/" target="_blank"><img src="images/footer_google.png" alt=""></a></div>
-            <div class="footer-social"><a href="http://linkedin.com/" target="_blank"><img src="images/footer_linkedin.png" alt=""></a></div>
-            <div class="footer-social"><a href="http://dribble.com/" target="_blank"><img src="images/footer_dribble.png" alt=""></a></div>
-            <div class="footer-social"><a href="http://pinterest.com/" target="_blank"><img src="images/footer_pinterest.png" alt=""></a></div>
-            <div class="footer-social"><a href="#" target="_blank"><img src="images/footer_rss.png" alt=""></a></div>
+            <div class="footer-social"><a href="http://facebook.com/" target="_blank"><img src="<?php print base_path() . drupal_get_path('theme', 'upc_pre') .'images/footer_facebook.png' ;?>" alt=""></a></div>
+            <div class="footer-social"><a href="http://twitter.com/" target="_blank"><img src="<?php print base_path() . drupal_get_path('theme', 'upc_pre') .'images/footer_twitter.png';?>" alt=""></a></div>
+            <div class="footer-social"><a href="http://google.com/" target="_blank"><img src="<?php print base_path() . drupal_get_path('theme', 'upc_pre') .'images/footer_google.png' ;?>" alt=""></a></div>
+            <div class="footer-social"><a href="http://linkedin.com/" target="_blank"><img src="<?php print base_path() . drupal_get_path('theme', 'upc_pre') .'images/footer_linkedin.png';?>" alt=""></a></div>
+            <div class="footer-social"><a href="http://dribble.com/" target="_blank"><img src="<?php print base_path() . drupal_get_path('theme', 'upc_pre') .'images/footer_dribble.png';?>" alt=""></a></div>
+            <div class="footer-social"><a href="http://pinterest.com/" target="_blank"><img src="<?php print base_path() . drupal_get_path('theme', 'upc_pre') .'images/footer_pinterest.png';?>" alt=""></a></div>
+            <div class="footer-social"><a href="#" target="_blank"><img src="<?php print base_path() . drupal_get_path('theme', 'upc_pre') .'images/footer_rss.png';?>" alt=""></a></div>
         
             <div class="copyright">        
                 <p>&copy; 2104 Sync. Derechos Reservados</p>        
