c31e8dde0937c35f300dd4ff879b4b93994b6f51
devildeveloper <devx.ry@gmail.com>
2014-03-05 00:52:25 -0500

    modificando marker de google maps

--- sites/all/themes/sync_theme/js/main.js
+++ sites/all/themes/sync_theme/js/main.js
@@ -179,21 +179,14 @@ function initialize() {
             var infowindow = new google.maps.InfoWindow({
                 content: contentString
             });
-            
-            var companyImage = new google.maps.MarkerImage('images/marker.png',
-                new google.maps.Size(58,63),<!-- Width and height of the marker -->
-                new google.maps.Point(0,0),
-                new google.maps.Point(35,20)<!-- Position of the marker -->
-            );
-    
+             
             
     
             var companyPos = new google.maps.LatLng(45.738028,21.224535);
     
             var companyMarker = new google.maps.Marker({
                 position: companyPos,
-                map: map,
-                icon: companyImage,               
+                map: map,             
                 title:"Eleven Media",
                 zIndex: 3});
             
