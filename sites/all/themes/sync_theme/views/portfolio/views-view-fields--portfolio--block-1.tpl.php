9574bfe1d8dda74652be39181cbffe98fabe13ab
Miguel Aliaga <devx.cry@gmail.com>
2014-03-07 10:59:32 -0500

    agregando cambios

--- sites/all/themes/sync_theme/views/portfolio/views-view-fields--portfolio--block-1.tpl.php
+++ sites/all/themes/sync_theme/views/portfolio/views-view-fields--portfolio--block-1.tpl.php
@@ -1,7 +1,7 @@
 <?php $nid= $fields['field_block_portfolio_content']->content ;?>
 <li class="view <?php print $fields['field_catgoria_de_portfolio']->content ;?>">
     <div class="view">
-        <a class="group1" href="<?php print '/sitio-drupal/node/'. $nid ;?>" title="Portfolio" target="_blank">
+        <a class="group1" href="<?php print '/node/'. $nid ;?>" title="Portfolio" target="_blank">
         	<img src="<?php print $fields['field_block_img']->content;?>" alt="">	 		
             <div class="mask">                    
                 <h5><?php print $fields['field_block_title']->content; ?></h5>
